82ae565a636056859bfe2df4781eeb35278d7811
KJS4588 <wjsdnd4588@gmail.com>
2020-09-03 15:39:33 +0900

    first commit

---
+++ include/lane_changer/lane_changer.h
@@ -0,0 +1,75 @@
+#include "ros/ros.h"
+
+#include "sensor_msgs/PointCloud2.h"
+#include "ackermann_msgs/AckermannDrive.h"
+#include "ackermann_msgs/AckermannDriveStamped.h"
+#include "geometry_msgs/Point.h"
+
+#include "pcl_conversions/pcl_conversions.h"
+#include "pcl_ros/point_cloud.h"
+#include "pcl_ros/transforms.h"
+
+#include "pcl/point_types.h"
+#include "pcl/io/pcd_io.h"
+#include "pcl/filters/conditional_removal.h"
+#include "pcl/filters/extract_indices.h"
+#include "pcl/filters/passthrough.h"
+
+#include "pcl/sample_consensus/model_types.h"
+#include "pcl/sample_consensus/method_types.h"
+#include "pcl/segmentation/sac_segmentation.h"
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/features/normal_3d.h>
+
+#include "visualization_msgs/Marker.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/tf.h"
+#include "geometry_msgs/Pose2D.h"
+
+#include "cmath"
+#include "math.h"
+#include "vector"
+#include "tuple"
+#include "std_msgs/Bool.h"
+#include "clustering.cpp"
+
+#define _USE_MATH_DEFINES
+#define DIST 5.0
+
+typedef pcl::PointXYZI PointType;
+
+class LaneChanger{
+
+private:
+	//ros
+    ros::NodeHandle nh_;
+
+	//publisher
+    ros::Publisher pub_;
+    ros::Publisher marker_pub_;
+
+	//subscriber
+    ros::Subscriber sub_;
+	ros::Subscriber acker_sub_;
+
+	//vector
+    vector<geometry_msgs::Point> local_obs_;
+
+	//various
+	double detectAngle_;
+	int isObsDetected_;
+
+public:
+    void initSetup();
+    void pointCallback(const sensor_msgs::PointCloud2ConstPtr &input);
+	void ackermannCallback(const ackermann_msgs::AckermannDriveStampedConstPtr &acker);
+	void visualize(vector<geometry_msgs::Point> obs_points);
+
+    double getDist(geometry_msgs::Point p);
+    double calcSteer(geometry_msgs::Point p);
+
+    LaneChanger() {
+        initSetup();
+    }
+};
+
---
+++ src/lane_changer.cpp
@@ -0,0 +1,77 @@
+#include "lane_changer/lane_changer.h"
+
+void LaneChanger::initSetup() {
+	pub_ = nh_.advertise<std_msgs::Bool>("/static_obs", 10);
+    marker_pub_ = nh_.advertise<visualization_msgs::Marker>("/output_points", 10);
+    sub_ = nh_.subscribe("/velodyne_points", 10, &LaneChanger::pointCallback, this);
+	acker_sub_ = nh_.subscribe("/ctrl_cmd", 10, &LaneChanger::ackermannCallback, this);
+	
+	isObsDetected_ = false;
+	detectAngle_ = 0;
+}
+
+
+//	0 : lane change when first obstacle detected 
+//  1 : ignore
+
+void LaneChanger::pointCallback(const sensor_msgs::PointCloud2ConstPtr &input) {
+	if(detectAngle_ > 10 && detectAngle_ < -10) {
+		local_obs_ = Cluster().cluster(input, 1, 13, -1, 1);
+		visualize(local_obs_);
+	}else { 
+		local_obs_ = Cluster().cluster(input, 1, 13, -1, 1);
+		visualize(local_obs_);
+
+		if(getDist(local_obs_.at(0)) < DIST) {
+			isObsDetected_ = true;	
+			std_msgs::Bool obs;
+			obs.data = isObsDetected_;
+			pub_.publish(obs);
+
+			isObsDetected_ = false;	
+		}	
+	}
+}
+
+
+void LaneChanger::ackermannCallback(const ackermann_msgs::AckermannDriveStampedConstPtr &acker){
+	detectAngle_ = acker->drive.steering_angle;
+}
+
+double LaneChanger::getDist(geometry_msgs::Point p){
+	return sqrt(pow(p.x - 0, 2) + pow(p.y - 0, 2));
+}
+
+void LaneChanger::visualize(vector<geometry_msgs::Point> input_points) {
+
+	visualization_msgs::Marker points;
+
+	points.header.frame_id = "velodyne";
+	points.header.stamp = ros::Time::now();
+	points.ns = "points_and_lines";
+	points.action = visualization_msgs::Marker::ADD;
+	points.pose.orientation.w = 1.0;
+	points.id = 1;
+	points.type = visualization_msgs::Marker::POINTS;
+	points.scale.x = 0.1; 
+	points.scale.y = 0.1;
+	points.color.a = 1.0;
+	points.color.g = 1.0f;
+
+	geometry_msgs::Point p;
+
+	for (auto point : input_points) {
+		p.x = point.x;
+		p.y = point.y;
+		p.z = point.z;
+		points.points.push_back(p);
+	}
+
+	marker_pub_.publish(points);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "lane_changer_node");
+    LaneChanger lc;
+	ros::spin();
+}
